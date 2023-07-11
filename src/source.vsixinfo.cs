e091fd153a5bf7cf8739c37212cdffe7822c3477
Tim Heuer <tim@timheuer.com>
2023-07-11 15:17:53 -0700

    Typos

--- src/source.vsixinfo.cs
+++ src/source.vsixinfo.cs
@@ -4,7 +4,7 @@
     {
         public const string Id = "TimHeuer.GitHubActions";
         public const string Name = "GitHub Actions";
-        public const string Description = @"A window that provides a view of GitHub Actions for the current repo of the opened solution in Visual StudioA ";
+        public const string Description = @"A window that provides a view of GitHub Actions for the current repo of the opened solution in Visual Studio";
         public const string Language = "en-US";
         public const string Author = "Tim Heuer";
         public const string Tags = "github,actions,workflow,devops,dotnet";
