0a6d739d812a357dd5e5a89c5571ca90bf76b135
Tim Heuer <tim@timheuer.com>
2023-07-31 16:56:35 -0700

    Checking for no value on secret

--- src/UserControls/AddEditSecret.xaml.cs
+++ src/UserControls/AddEditSecret.xaml.cs
@@ -20,17 +20,27 @@ public partial class AddEditSecret : Window
     public string SecretName => txtName.Text.Trim();
     public string SecretValue => txtSecret.Text.Trim();
 
-    private void Save_Click(object sender, RoutedEventArgs e)
+    private async void Save_Click(object sender, RoutedEventArgs e)
     {
         // Secret names can only contain alphanumeric characters ([a-z], [A-Z], [0-9]) or underscores (_). Spaces are not allowed. Must start with a letter ([a-z], [A-Z]) or underscores (_).
         Regex rx = new Regex("^[a-zA-Z_][a-zA-Z0-9_]*$");
         if (rx.IsMatch(txtName.Text))
         {
-            DialogResult = true;
-            Close();
+            if (string.IsNullOrWhiteSpace(txtSecret.Text.Trim()))
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                Community.VisualStudio.Toolkit.MessageBox mb = new();
+                mb.ShowError("Secret value cannot be empty.", "Invalid Secret Value");
+            }
+            else
+            {
+                DialogResult = true;
+                Close();
+            }
         }
         else
         {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
             Community.VisualStudio.Toolkit.MessageBox mb = new();
             mb.ShowError("Secret names can only contain alphanumeric characters ([a-z], [A-Z], [0-9]) or underscores (_). Spaces are not allowed. Must start with a letter ([a-z], [A-Z]) or underscores (_).", "Invalid Secret Name");
         }
