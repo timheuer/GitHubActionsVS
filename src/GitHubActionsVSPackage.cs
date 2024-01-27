8c9618a03e21ea32845c46c6b45d8de71386204f
Tim Heuer <tim@timheuer.com>
2024-01-26 16:05:49 -0800

    Trying better versioning for prod registration

--- src/GitHubActionsVSPackage.cs
+++ src/GitHubActionsVSPackage.cs
@@ -10,7 +10,7 @@ using System.Threading;
 
 namespace GitHubActionsVS;
 [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Id)]
+[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
 [ProvideToolWindow(typeof(ActionsToolWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
 [ProvideOptionPage(typeof(OptionsProvider.ExtensionOptionsOptions), "GitHub", "Actions", 0, 0, true, SupportsProfiles = true)]
 [ProvideMenuResource("Menus.ctmenu", 1)]
